d04898054cd9c126a4c6c08c752866fd3e0e7cee
andre_fandrade <kodec_andrade@hotmail.com>
2021-06-22 23:22:06 -0400

    Trabalhando com Arrays

--- React_Intro_Basico/hello-world/src/App.js
+++ React_Intro_Basico/hello-world/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
@@ -18,36 +18,67 @@ class App extends Component {
 
   // Utilizando Class Fields
   state = {
-    name: 'Andre Felipe',
-    age: 27,
-    counter: 0
+
+    // name: 'Andre Felipe',
+    // age: 27,
+    // counter: 0
+
+    posts: [
+      {
+        id: 1,
+        title: 'Titulo 1',
+        body: 'Corpo 1'
+      },
+      {
+        id: 2,
+        title: 'Titulo 2',
+        body: 'Corpo 2'
+      },
+      {
+        id: 3,
+        title: 'Titulo 3',
+        body: 'Corpo 3'
+      },
+    ]
+
   }
 
   // Utilizando o Arrow Functions para não precisar chamar this.
-  handlePclick = () => {
-    const { age } = this.state;
+  // handlePclick = () => {
+  //   const { age } = this.state;
 
-    console.log(`My age is: ${age}`);
-    document.querySelector('.age-cl').innerHTML = `My age is: ${age}`;
+  //   console.log(`My age is: ${age}`);
+  //   document.querySelector('.age-cl').innerHTML = `My age is: ${age}`;
 
-    this.setState({ age: age + 1 });
-  }
+  //   this.setState({ age: age + 1 });
+  // }
 
-  handleAclick = (event) => {
-    // Usando Eventos
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 })
-  }
+  // handleAclick = (event) => {
+  //   // Usando Eventos
+  //   event.preventDefault();
+  //   const { counter } = this.state;
+  //   this.setState({ counter: counter + 1 })
+  // }
 
   render() {
 
-    const { name, counter } = this.state;
+    // const { name, counter } = this.state;
+    const { posts } = this.state;
 
     return (
 
       <div className="App">
-        <header className="App-header">
+
+        {/* Trabalhando com Arrays */}
+
+        {posts.map(post => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
 
           <p>My name is: {name}</p>
@@ -60,7 +91,8 @@ class App extends Component {
           >
             Learn React {counter}
           </a>
-        </header>
+        </header> */}
+
       </div>
 
     );
