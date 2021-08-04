029d178378a45be4e83d42c361d033364f047057
andre-fandrade <kodec_andrade@hotmail.com>
2021-08-04 15:03:58 -0400

    Atualização

--- Dapp-Testing/metamask-connect/src/utils/connect-wallet.js
+++ Dapp-Testing/metamask-connect/src/utils/connect-wallet.js
@@ -3,30 +3,60 @@ import {checkExtension} from "./check-extension";
 
 export class ConnectWallet extends Component {
 
+  state = {
+    walletConnected: window.ethereum._state.isConnected
+  }
+
+  componentDidMount() {
+    this.eth()
+    this.isConnect()
+  }
+
+  eth = () => {
+    const {ethereum} = window
+    return ethereum
+  }
+
+  isConnect = () => {
+    const address = this.eth().selectedAddress
+    if (!address === '') {
+      this.setState({walletConnected: true})
+    }
+  }
+
+  connect = async () => {
+    await this.eth().request({method: "eth_requestAccounts"})
+      .catch(error => {
+        const {message} = error
+        alert(`Error: ${message}`)
+      })
+  }
+
   render() {
 
     const {textBtn, status, link} = checkExtension()
+    const {walletConnected} = this.state
 
-
-    if (status) {
-      async function connect() {
-        const {ethereum} = window
-        await ethereum.request({method: "eth_requestAccounts"})
-          .catch(error => {
-            const {message} = error
-            console.log(`Error: ${message}`)
-          })
-      }
-
+    if (status && !walletConnected) {
       return (
         <div>
-          <button onClick={connect}>{textBtn}</button>
+          <button onClick={this.connect}>{textBtn}</button>
         </div>
       )
-    } else {
+    }
+
+    if (!status && walletConnected) {
       return (
         <div>
           <a href={link}>{textBtn}</a>
+        </div>
+      )
+    }
+
+    if (walletConnected) {
+      return (
+        <div>
+          <h2>{this.eth().selectedAddress}</h2>
         </div>
       )
     }
