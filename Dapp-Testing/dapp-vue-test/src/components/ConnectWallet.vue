6e397e628f29a9c0e6d5a46db211165d34b4f753
andre-fandrade <kodec_andrade@hotmail.com>
2021-08-05 13:26:18 -0400

    Testando.

---
+++ Dapp-Testing/dapp-vue-test/src/components/ConnectWallet.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="Connect">
+      <p> ok google! {{nome}}</p>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'ConnectWallet',
+
+
+   props: {
+      data: '.Connect' {
+         nome: 'dffffdds'
+      }
+   }
+}
+</script>
+
+<style scoped>
+
+</style>
