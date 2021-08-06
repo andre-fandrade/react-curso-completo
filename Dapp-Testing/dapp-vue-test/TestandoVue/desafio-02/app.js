bb3395ddc09707a77d97a619ae5335a3a3d94aa5
andre-fandrade <kodec_andrade@hotmail.com>
2021-08-06 15:43:39 -0400

    Desafio 2 e conclusão dos eventos.

---
+++ Dapp-Testing/dapp-vue-test/TestandoVue/desafio-02/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+        valor: 0
+    },
+    methods: {
+        cliqueDoBotao: function () {
+            alert('Você clicou no botão!')
+        }
+    }
+})
