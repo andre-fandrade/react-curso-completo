fd40e710fb1a6c4bde11b2600eaa604978ab24af
andre-fandrade <kodec_andrade@hotmail.com>
2021-08-06 15:57:21 -0400

    Concluindo o desafio.

--- Dapp-Testing/dapp-vue-test/TestandoVue/desafio-02/app.js
+++ Dapp-Testing/dapp-vue-test/TestandoVue/desafio-02/app.js
@@ -1,11 +1,20 @@
 new Vue({
-    el: '#app',
+    el: '#desafio',
     data: {
-        valor: 0
+        valor: 0,
+        valor2: 0
     },
     methods: {
         cliqueDoBotao: function () {
             alert('Você clicou no botão!')
+        },
+        OuvirEventoKeyDown: function (e) {
+            // Pegar o valor que esta sendo digitado.
+            this.valor = e.target.value
+        },
+        OuvirEventoKeyDown2: function (e) {
+            // Pegar o valor que esta sendo digitado.
+            this.valor2 = e.target.value
         }
     }
 })
