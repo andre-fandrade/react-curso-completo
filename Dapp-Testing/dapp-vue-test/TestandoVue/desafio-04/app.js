196baf7186fd98d6e3319db75d477396752da06a
andre-fandrade <kodec_andrade@hotmail.com>
2021-08-08 19:36:40 -0400

    ...

--- Dapp-Testing/dapp-vue-test/TestandoVue/desafio-04/app.js
+++ Dapp-Testing/dapp-vue-test/TestandoVue/desafio-04/app.js
@@ -1,16 +1,27 @@
 new Vue({
-	el: '#desafio',
-	data: {
-		encolher: 'encolher',
-		destaque: 'destaque',
-		iniciarEfeito: false
-	},
-	methods: {
-		iniciarProgresso() {
+  el: '#desafio',
+  data: {
+    encolher: 'encolher',
+    destaque: 'destaque',
+    iniciarEfeito: false,
+		font: 'font',
+		color: 'color'
+  },
+  methods: {
+    iniciarProgresso() {
 
+    }
+  },
+  computed: {
+    estiloUm() {
+      return {
+        fontSize: '30px'
+      }
+    },
+		estiloDois() {
+    	return {
+    		color: 'red'
+			}
 		}
-	},
-	computed: {
-
-	}
+  }
 })
