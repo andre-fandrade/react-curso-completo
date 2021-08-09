a80fcfe4078e331673ba1fc0f27967be486f8836
andre-fandrade <kodec_andrade@hotmail.com>
2021-08-08 20:44:39 -0400

    Desafio concluído.

--- Dapp-Testing/dapp-vue-test/TestandoVue/desafio-04/app.js
+++ Dapp-Testing/dapp-vue-test/TestandoVue/desafio-04/app.js
@@ -4,12 +4,24 @@ new Vue({
     encolher: 'encolher',
     destaque: 'destaque',
     iniciarEfeito: false,
-		font: 'font',
-		color: 'color'
+    font: 'font',
+    color: 'color',
+    exemplo: 'exemplo destaque',
+    exUm: '',
+    exDois: false,
+    progresso: 10,
+    barraProgresso: 'width: 1px'
   },
   methods: {
     iniciarProgresso() {
+      const tempo = setInterval(() => {
+        this.progresso++
+        this.barraProgresso = `width: ${this.progresso}%`
 
+        if (this.progresso == 100) {
+          clearInterval(tempo)
+        }
+      }, 1000)
     }
   },
   computed: {
@@ -18,10 +30,17 @@ new Vue({
         fontSize: '30px'
       }
     },
-		estiloDois() {
-    	return {
-    		color: 'red'
-			}
-		}
+    estiloDois() {
+      return {
+        color: 'red'
+      }
+    },
+    estiloTres() {
+      return {
+        'background-color': 'gray',
+        width: '50px !important',
+        height: '50px',
+      }
+    },
   }
 })
