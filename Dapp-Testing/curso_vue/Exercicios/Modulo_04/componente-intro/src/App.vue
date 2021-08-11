f05713ab5982e473630fc82bebebdcc3bcb28c01
andre-fandrade <kodec_andrade@hotmail.com>
2021-08-11 15:38:44 -0400

    Adicionando primeiro projeto.

---
+++ Dapp-Testing/curso_vue/Exercicios/Modulo_04/componente-intro/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+   <h1>Contadores</h1>
+   {{contador}}
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         contador: 0
+      }
+   }
+}
+</script>
+
+<style>
+
+
+</style>
