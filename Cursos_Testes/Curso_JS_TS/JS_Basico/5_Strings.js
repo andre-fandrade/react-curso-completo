8457651f898fb7d006655280a802a0669042192b
andre-fandrade <kodec_andrade@hotmail.com>
2021-10-07 10:00:43 -0400

    ...

---
+++ Cursos_Testes/Curso_JS_TS/JS_Basico/5_Strings.js
@@ -0,0 +1,13 @@
+function log(params) {
+  console.log('====================================');
+  console.log(params);
+  console.log('====================================');
+}
+
+// Forma de Escapar uma string:
+let umaString = "Um \"Texto\"";
+
+
+// Strings tem índice:
+umaString
+
